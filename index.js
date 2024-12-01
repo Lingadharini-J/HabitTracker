c16bc2e0a5bc1faead801ea0c2cba2a6b5b395f1
Lingadharini-J <159918732+Lingadharini-J@users.noreply.github.com>
2024-12-01 09:25:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const http = require('http');
-const express = require('express'); // requeiring express
+const express = require('express'); // requiring express
 const bodyParser = require('body-parser');
 const port =  8000; // assigning the port, for running in local computer
-const expressLayout=require('express-ejs-layouts');
+const expressLayout = require('express-ejs-layouts');
 const app = express();
-// require the connect flash, to show the notification in flash msgs
-// they are used for showing action notifications
-const flash = require('connect-flash'); 
-const flashMiddleWare = require('./config/flashMiddleware');
 
-const db = require('./config/moongoose'); // require the database
+// require the connect flash, to show notifications in flash messages
+const flash = require('connect-flash'); 
+const flashMiddleware = require('./config/flashMiddleware'); // Correcting the path
+
+const db = require('./config/mongoose'); // require the database
 
 // used for session cookies
 const session = require("express-session");
@@ -17,18 +17,18 @@ const passport = require('passport');
 const passportLocal = require('./config/passport_local');
 
 // Setting up the view engine
-app.set('view engine','ejs');
-app.set('views','./views');
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
 app.use(expressLayout);
-app.use(bodyParser.urlencoded({extended:false})); // since express does not have bodyPaser with it, so we have to download , and use it as middle ware
+app.use(bodyParser.urlencoded({ extended: false })); // since express does not have bodyParser with it, so we have to download, and use it as middleware
 
 app.use(express.static('./assets'));
 
-//mongo store is used to store the session cookie
+// mongo store is used to store the session cookie
 app.use(session({
     name: 'codeial',
-    // TODO change the secret before deployment in production mode
+    // TODO: change the secret before deployment in production mode
     secret: "habitTracker",
     saveUninitialized: false,
     resave: false,
@@ -44,16 +44,16 @@ app.use(passport.setAuthenticatedUser);
 
 // Using Connect flash
 app.use(flash());
-app.use(flashMiddleWare.setFlash);
+app.use(flashMiddleware.setFlash); // Apply the flash middleware to capture messages
 
 app.use('/' , require('./routes/index')); // middleware for router
 
-// directing the app in the given port
-app.listen(port, function(err){
-    if(err){
-        console.log(err );
-        return ;
+// directing the app to the given port
+app.listen(port, function(err) {
+    if (err) {
+        console.log(err);
+        return;
     }
     
-    console.log(`Server is up and running in port ${port}`);
-})
+    console.log(`Server is up and running on port ${port}`);
+});
